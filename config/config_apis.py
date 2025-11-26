ce4b5438fa180e04bcb0ea27aed10c99112a5a2e
erikandgabriel <eeu31471@gmail.com>
2025-11-26 00:04:13 -0300

    Add configuration for multiple APIs

---
+++ config/config_apis.py
@@ -0,0 +1,17 @@
+APIS = {
+    "api_ip": {
+        "nome": "ip-api",
+        "url": "http://ip-api.com/json/{}",
+        "tempo": 10,
+    },
+    "api_cep": {
+        "nome": "cep.awesomeapi",
+        "url": "https://cep.awesomeapi.com.br/json/{}",
+        "tempo": 10
+    },
+    "api_cnpj": {
+        "nome": "brasilapi",
+        "url": "https://brasilapi.com.br/api/cnpj/v1/{}",
+        "tempo": 10
+    }
+}
