a8e1f25720ed5d29925f2fe68e2ef3bd4403b31d
erikandgabriel <eeu31471@gmail.com>
2025-11-29 01:04:39 -0300

    Update ip.py

--- src/servicos/ip.py
+++ src/servicos/ip.py
@@ -3,6 +3,9 @@ import json
 from config.config_apis import APIS 
 from config.config_app import APP 
 from style.banners import Banner
+from colorama import Fore, init
+
+init(autoreset=True)
 
 class BuscarIp:
     def __init__(self, ip, arquivo):
@@ -18,20 +21,20 @@ class BuscarIp:
         if resposta.status_code == 200:
             dados = resposta.json()
             CAMPOS = {
-                "status": dados["status"],
-                "country": dados["country"],
-                "countryCode": dados["countryCode"],
-                "region": dados["region"],
-                "regionName": dados["regionName"],
-                "city": dados["city"],
-                "zip": dados["zip"],
-                "lat": dados["lat"],
-                "lon": dados["lon"],
-                "timezone": dados["timezone"],
-                "isp": dados["isp"],
-                "org": dados["org"],
-                "as": dados["as"],
-                "query": dados["query"]
+                f"{Fore.RED}status": Fore.BLUE + dados["status"],
+                f"{Fore.RED}country": Fore.BLUE + dados["country"],
+                f"{Fore.RED}countryCode": Fore.BLUE + dados["countryCode"],
+                f"{Fore.RED}region": Fore.BLUE + dados["region"],
+                f"{Fore.RED}regionName": Fore.BLUE + dados["regionName"],
+                f"{Fore.RED}city": Fore.BLUE + dados["city"],
+                f"{Fore.RED}zip": Fore.BLUE + dados["zip"],
+                f"{Fore.RED}lat": Fore.BLUE + str(dados["lat"]),
+                f"{Fore.RED}lon": Fore.BLUE + str(dados["lon"]),
+                f"{Fore.RED}timezone": Fore.BLUE + dados["timezone"],
+                f"{Fore.RED}isp": Fore.BLUE + dados["isp"],
+                f"{Fore.RED}org": Fore.BLUE + dados["org"],
+                f"{Fore.RED}as": Fore.BLUE + dados["as"],
+                f"{Fore.RED}query": Fore.BLUE + dados["query"]
             }
 
             for chave, valor in CAMPOS.items():
@@ -41,20 +44,19 @@ class BuscarIp:
             return self.dados
 
         else:
-            print("\nholve um erro!!")
+            print(f"\n{Fore.RED}holve um erro!!")
     
 class SalvarIp(BuscarIp):
     def __init__(self, ip, arquivo):
         super().__init__(ip, arquivo)
 
-    def SalvarIp(self):
+    def Salvar(self):
         if self.dados is None:
             self.Buscar()
 
         if self.dados is not None:
             with open(f"{self.arquivo}", "w") as arq:
                 json.dump(self.dados, arq, indent=4)
-            print(f"\narquivo salvo em {self.arquivo}")
+            print(f"\n{Fore.GREEN}arquivo salvo em {Fore.YELLOW}{self.arquivo}")
         else:
-            print("\nNao a dados para salvar.")
-
+            print(f"\n{Fore.RED}Nao a dados para salvar.")
