d73c443b3a8a4cdc50be18ad7f762b751743a04a
erikandgabriel <eeu31471@gmail.com>
2025-11-29 01:05:05 -0300

    Add BuscarCnpj class for CNPJ lookup

--- src/servicos/cnpj.py
+++ src/servicos/cnpj.py
@@ -1,1 +1,17 @@
+import requests 
+import json 
+from style.banners import Banner
+from config.config_apis import APIS 
+from config.config_app import APP 
 
+class BuscarCnpj:
+    def __init__(self, cnpj, arquivo):
+        self.cnpj = cnpj 
+        self.arquivo = arquivo
+        self.dados = None 
+
+    def Buscar(self):
+        api = APIS["api_cnpj"]
+        url = api["url"].format(self.cnpj)
+
+        resposta 
